bccafb8f6c9a918f327ee9c8450458e5fb9227b1
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-03-13 19:45:02 +0900

    completed Bullet function & Bullet Box function

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public int Bullet;
+	public int bulletBox;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0) && Bullet != 0) {
+			print("左ボタンが押されている");
+			Bullet -= 1;
+			Debug.Log (Bullet);
+			if(Bullet == 0){
+				print ("なくなりました！");
+				return;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.R) && Bullet == 0) {
+			Bullet = 3;
+			bulletBox -= Bullet;
+			print (bulletBox);
+		}
+//
+//		if(bulletBox == 0){
+//			print ("なくなりました！");
+//		}
+	}
+}
