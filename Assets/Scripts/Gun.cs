a866de1b945f6336cd743c41fdddbed199cffbc7
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-03-15 18:25:28 +0900

    added the Reload Function

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,38 +3,60 @@ using System.Collections;
 
 public class Gun : MonoBehaviour {
 
+	//銃に入れれる弾の限界数
 	public int Bullet;
+
+	//弾倉
 	public int bulletBox;
 	float coolTime;
 
-	//銃声
-	[SerializeField] AudioClip gunfireSound;
+	//残弾数・リロードする弾数
+	int residualBullet, reloadBullet;
+
+	//銃声・リロード音
+	[SerializeField] AudioClip gunfireSound, reloadSound;
 	AudioSource audioSource;
 
 	void Start () {
+		//残弾数 = 初期弾数
+		residualBullet = Bullet;
 		audioSource = transform.GetComponent<AudioSource> ();
 	}
 	
 	void Update () {
+		//クーリング
 		coolTime -= Time.deltaTime;
-		if (Input.GetMouseButton(0) && Bullet != 0) {
 
+		//リロードする弾数
+		reloadBullet = Bullet - residualBullet;
+
+		//クリックしてショットする
+		if (Input.GetMouseButton(0) && residualBullet != 0) {
 			//クールタイム機能
 			if (coolTime <= 0f){
 				Shoot();
 			}
 		}
 
-		if (Input.GetKeyDown (KeyCode.R) && Bullet == 0) {
-			Bullet = 3;
-			bulletBox -= Bullet;
-			print (bulletBox);
+		//リロード機能
+		if (Input.GetKeyDown (KeyCode.R) && residualBullet < Bullet) {
+			audioSource.PlayOneShot (reloadSound);
+			//残弾数 = 初期弾数
+			residualBullet += reloadBullet;
+			//リロードすると弾倉内弾数が減る
+			bulletBox -= reloadBullet;
+
+			print ("弾倉内弾数：" + bulletBox);
 		}
 	}
 
+	//ショット機能
 	void Shoot () {
-		Bullet -= 1;
-		Debug.Log ("残弾数：" + Bullet);
+		residualBullet -= 1;
+		//後ほどUIで使用
+		Debug.Log ("残弾数：" + residualBullet);
+
+		//クールタイム設定
 		coolTime = 0.5f;
 		audioSource.PlayOneShot (gunfireSound);
 	}
