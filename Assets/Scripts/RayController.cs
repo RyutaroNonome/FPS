3fc70045e3b0cd6be61f669e50004fae01220169
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-03-15 13:32:46 +0900

    added a RayController script

---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayController : MonoBehaviour {
+
+	void start () {
+	}
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+//			Ray ray = Camera.main.ScreenPointToRay(Camera.main.transform.forward);
+			Ray ray = new Ray(transform.position, transform.forward);
+
+			RaycastHit hit = new RaycastHit();
+
+			if (Physics.Raycast(ray, out hit)) {
+				GameObject selectedObj = hit.collider.gameObject;             
+				print("name: " + selectedObj.name + selectedObj.transform.position);
+			}
+		}
+	}
+}
