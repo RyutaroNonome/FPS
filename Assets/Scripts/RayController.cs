588828f471712ddd9dfa292df54a22ade415f7ae
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-03-15 17:19:07 +0900

    added the Muzzle Effect Function

--- Assets/Scripts/RayController.cs
+++ Assets/Scripts/RayController.cs
@@ -6,27 +6,30 @@ public class RayController : MonoBehaviour {
 	//パーティクル
 	public GameObject gunfireEffect;
 	[SerializeField] GameObject usingGun;
+	[SerializeField] GameObject Muzzle;
 	Vector3 muzzlePoint;
+
+	//パーティクル発生
+	GameObject cloneHitEffect, cloneMuzzleEffect;
 
 	void Update () {
 		if(Input.GetMouseButtonDown(0)){
 			Ray ray = new Ray(transform.position, transform.forward);
 
 			RaycastHit hit = new RaycastHit();
+			muzzlePoint = Muzzle.transform.position;
+			cloneMuzzleEffect = (GameObject)Instantiate (gunfireEffect, muzzlePoint, Quaternion.identity);
+
+			//パーティクル削除
+			Destroy (cloneMuzzleEffect, .5f);
 
 			if (Physics.Raycast(ray, out hit)) {
 				GameObject selectedObj = hit.collider.gameObject;
 
-				//パーティクル発生
-				GameObject clone1, clone2;
-				muzzlePoint = new Vector3 (usingGun.transform.position.x, usingGun.transform.position.y , usingGun.transform.position.z + 1f);
-
-				clone1 = (GameObject)Instantiate (gunfireEffect, hit.point, Quaternion.identity);
-				clone2 = (GameObject)Instantiate (gunfireEffect, muzzlePoint, Quaternion.identity);
+				cloneHitEffect = (GameObject)Instantiate (gunfireEffect, hit.point, Quaternion.identity);
 
 				//パーティクル削除
-				Destroy (clone1, .5f);
-				Destroy (clone2, .5f);
+				Destroy (cloneHitEffect, .5f);
 			}
 		}
 	}
